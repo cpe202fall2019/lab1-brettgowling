c825653cf32daa363dd2f688a1fc308fab2fd9db
Brett Gowling <bgowling@calpoly.edu>
2019-09-25 10:12:11 -0700

    Added repr test 2

--- location_tests.py
+++ location_tests.py
@@ -7,7 +7,9 @@ class TestLab1(unittest.TestCase):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
     
-    # Add more tests!
+    def test_repr2(self):
+        loc = Location("Bakesfield", 123, -456)
+        self.assertEqual(repr(loc), "Location('Bakersfield', 123, -456)")
 
 if __name__ == "__main__":
         unittest.main()
