383cd7e10efb723ab60d9a770c6d9daa3182d205
Brett Gowling <bgowling@calpoly.edu>
2019-09-25 10:28:35 -0700

    Switched order of if statements

--- lab1.py
+++ lab1.py
@@ -2,10 +2,10 @@
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
+   if int_list == None:
+       raise ValueError
    if len(int_list) == 0:
       return None
-   if int_list == None:
-      raise ValueError
    max = int_list[0]
    for i in range(len(int_list)):
       if int_list[i] > max:
