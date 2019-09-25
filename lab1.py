84ccc3263352d7044a0eb33d23dd6109bcfa74c5
Brett Gowling <bgowling@calpoly.edu>
2019-09-25 11:01:55 -0700

    Started reversed list code

--- lab1.py
+++ lab1.py
@@ -15,6 +15,13 @@ def max_list_iter(int_list):  # must use iteration not recursion
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
+   if int_list == None:
+      raise ValueError
+   if len(int_list) == 0:
+      return None
+   if i == len(int_list):
+      return rec_list
+
    pass
 
 def bin_search(target, low, high, int_list):  # must use recursion
