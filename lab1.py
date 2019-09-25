97382f3aa1664d2adbc9929cb3dfe09cce3b5b94
Brett Gowling <bgowling@calpoly.edu>
2019-09-25 13:16:26 -0700

    Trial 1 at recursive return statement

--- lab1.py
+++ lab1.py
@@ -1,30 +1,29 @@
-
 def max_list_iter(int_list):  # must use iteration not recursion
-   """finds the max of a list of numbers and returns the value (not the index)
+    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   if int_list == None:
-       raise ValueError
-   if len(int_list) == 0:
-      return None
-   max = int_list[0]
-   for i in range(len(int_list)):
-      if int_list[i] > max:
-         max = int_list[i]
-   return max
+    if int_list == None:
+        raise ValueError
+    if len(int_list) == 0:
+        return None
+    max = int_list[0]
+    for i in range(len(int_list)):
+        if int_list[i] > max:
+            max = int_list[i]
+    return max
 
-def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
+
+def reverse_rec(int_list, rec_list = []):  # must use recursion
+    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   if int_list == None:
-      raise ValueError
-   if len(int_list) == 0:
-      return None
-   if i == len(int_list):
-      return rec_list
+    if int_list == None:
+        raise ValueError
+    if len(int_list) == 0:
+        return None
+    rec_list.append(int_list.pop())
+    return reverse_rec(int_list, rec_list)
 
-   pass
 
 def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
+    """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
-   pass
+    pass
